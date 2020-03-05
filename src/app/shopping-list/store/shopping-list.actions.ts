4ed3911d74d952716f70582572f2b1130cae5e92
krompac <tomijehrvat@gmail.com>
2020-03-05 09:06:21 +0100

    Refactored action identifiers

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,14 +1,14 @@
 import {Action} from '@ngrx/store';
 import {Ingredient} from '../../shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const ADD_INGREDIENT = '[Shopping list] Add Ingredient';
+export const ADD_INGREDIENTS = '[Shopping list] Add Ingredients';
 
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const UPDATE_INGREDIENT = '[Shopping list] Update Ingredient';
+export const START_EDIT = '[Shopping list] Start Edit';
+export const STOP_EDIT = '[Shopping list] Stop Edit';
 
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const DELETE_INGREDIENT = '[Shopping list] Delete Ingredient';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
